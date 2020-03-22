0eaad1bdedc67deb39678152fea05ef895595649
srchd <sipirichi@gmail.com>
2020-03-22 16:20:21 +0100

    fully works, i just have to put more cities in

--- vakterkep.py
+++ vakterkep.py
@@ -24,6 +24,7 @@ class Countries:
 
 class WelcomeWindow:
     def __init__(self, master):
+        self.started = False
         self.master = master
         master.title("Vaktérkép")
 
@@ -42,13 +43,14 @@ class WelcomeWindow:
         self.germany = Radiobutton(master, text="Németország", variable=self.var, value=3)
         self.germany.pack(anchor=W)
         
-        self.quit_button = Button(master, text="Quit", command=master.destroy)
-        self.quit_button.pack()
-
         self.confirm_button = Button(master, text="Confirm", command=self.confirm)
         self.confirm_button.pack()
 
+        self.quit_button = Button(master, text="Quit", command=master.destroy)
+        self.quit_button.pack()
+
     def confirm(self):
+        self.started = True
         self.width = 1152
         self.height = 648
         #choice = self.var.get()
@@ -68,17 +70,17 @@ class WelcomeWindow:
         else:
             print("Nem valasztottal orszagot!")
 
-#oe_cities = namedtuple("oe_cities", "city cpos minpos maxpos")
-oe_cities = [
-                [["Vorarlberg - Bregenz"], [(54, 371)], [(44,361)], [(64,381)]],
-                [["Tirol – Innsbruck"], [(272,425)], [(262,415)], [(282,435)]]
-            ]
-
 def is_correct(pos, min_pos, max_pos):
     if pos >= min_pos and pos <= max_pos:
         return True
     else:
         return False
+
+def finished(succ):
+    for x in succ:
+        if x == 0:
+            return False
+    return True
 
 def main():
         root = Tk()
@@ -86,56 +88,123 @@ def main():
         root.geometry("400x250+300+300")
         root.mainloop()
 
+        if not mygui.started:
+            return
+
         run = True
 
         choice = mygui.var.get()
  
+        RED = (255, 0, 0)
+        GREEN = (0, 255, 0)
+        BLACK = (0, 0, 0)
+        font = pygame.font.Font(os.path.join("assets","LEMONMILK-Medium.otf"), 32)
+
         if choice == 1:
             width = 1152
             height = 648
+            c_range = 10
+            cities = [
+                [["Zürich - Zürich"], [(651, 140)], [(651 - c_range, 140 - c_range)], [(651 + c_range, 140 + c_range)]],
+                [["Bern – Bern"], [(377, 272)], [(377 - c_range, 272 - c_range)], [(377 + c_range, 272 + c_range)]]
+            ]
+            in_range = len(cities)
+            print("in_range:{}",in_range)
+            succ = [0] * in_range
 
             country = Countries(width, height, "schweiz.jpg")
+
         elif choice == 2:
             width = 1152
             height = 648
+            c_range = 10
+            cities = [
+                [["Vorarlberg - Bregenz"], [(54, 371)], [(54 - c_range, 371 - c_range)], [(54 + c_range, 371 + c_range)]],
+                [["Tirol – Innsbruck"], [(272, 425)], [(272 - c_range, 425 - c_range)], [(272 + c_range, 425 + c_range)]]
+            ]
+            in_range = len(cities)
+            print("in_range:{}",in_range)
+            succ = [0] * in_range
 
             country = Countries(width, height, "oesterreich.png")
 
         elif choice == 3:
             width = 682
             height = 780
+            c_range = 15
+            cities = [
+                [["Bayern – München"], [(426, 689)], [(426 - c_range, 689 - c_range)], [(426 + c_range, 689 + c_range)]],
+                [["Baden-Württemberg"], [(249, 624)], [(249 - c_range, 624 - c_range)], [(249 + c_range, 624 + c_range)]]
+            ]
+            in_range = len(cities)
+            print("in_range:{}",in_range)
+            succ = [0] * in_range
 
             country = Countries(width, height, "deutschland.png")
 
-        RED = (255, 0, 0)
-        GREEN = (0, 255, 0)
-        BLACK = (0, 0, 0)
         r = random.SystemRandom()
-        city = r.choice(range(2))
-        font = pygame.font.Font(os.path.join("assets","LEMONMILK-Medium.otf"), 32)
-        text = font.render(oe_cities[city][0][0], True, BLACK)
+        city = r.choice(range(in_range))
+
+        text = font.render(cities[city][0][0], True, BLACK)
         textRect = text.get_rect()
-        textRect.center = (country.width / 2, 20)
+        if choice == 2:
+            textRect.center = (int(country.width / 2), 20)
+        else:
+            textRect.topleft = (8, 8)
+
+        snapshot = country.win.copy()
+
+        score = 0
+
         while run:
-            country.win.blit(text, textRect)
+            to_remove = country.win.blit(text, textRect)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
                     print(pos)
-                    if is_correct(pos, oe_cities[city][2][0], oe_cities[city][3][0]):
-                        pygame.draw.circle(country.win, GREEN, oe_cities[city][1][0], 5, 0)
-                        city = r.choice(range(2))
-                        text = font.render(oe_cities[city][0][0], True, BLACK)
-                        textRect = text.get_rect()
-                        textRect.center = (country.width / 2, 20)
+                    if is_correct(pos, cities[city][2][0], cities[city][3][0]):
+                        succ[city] = 1
+                        country.win.blit(snapshot, to_remove, to_remove)
+                        pygame.draw.circle(country.win, GREEN, cities[city][1][0], 5, 0)
+                        score += 1
+                        if not finished(succ):
+                            temp_city = r.choice(range(in_range))
+                            while temp_city == city:
+                                temp_city = r.choice(range(in_range))
+                            city = temp_city
+                            text = font.render(cities[city][0][0], True, BLACK)
+                            textRect = text.get_rect()
+                            if choice == 2:
+                                textRect.center = (int(country.width / 2), 20)
+                            else:
+                                textRect.topleft = (8, 8)
+                        else:
+                            run = False
                     else:
-                        pygame.draw.circle(country.win, RED, oe_cities[city][1][0], 5, 0)
+                        succ[city] = 1
+                        country.win.blit(snapshot, to_remove, to_remove)
+                        pygame.draw.circle(country.win, RED, cities[city][1][0], 5, 0)
+                        if not finished(succ):
+                            temp_city = r.choice(range(in_range))
+                            while temp_city == city:
+                                temp_city = r.choice(range(in_range))
+                            city = temp_city
+                            text = font.render(cities[city][0][0], True, BLACK)
+                            textRect = text.get_rect()
+                            if choice == 2:
+                                textRect.center = (int(country.width / 2), 20)
+                            else:
+                                textRect.topleft = (8, 8)
+                        else:
+                            run = False
             if choice == 0:
                 run = False
             pygame.display.update()
         pygame.quit()
+        if finished(succ):
+            print(score, "/", in_range) # majd tk_interbe írja ki, konzol nélküli program lesz
 
 if __name__ == '__main__':
     main()
